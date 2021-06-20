571c7a529232ed7e31c80f578a295c480f933965
Sirok Eszter <sirokeszter@gmail.com>
2021-06-20 12:24:22 +0200

    Python homework

--- python-homework/charsandord.py
+++ python-homework/charsandord.py
@@ -4,3 +4,22 @@ sor3=list(range(118,129))
 
 for i in range(0,len(sor1)):
     print(chr(sor1[i]),sor1[i],chr(sor2[i]),sor2[i],chr(sor3[i]),sor3[i])
+
+print("*"*20) # elválasztásképpen
+
+# Másik megoldás:
+my_list=[97,107,118]
+for i in range(10):
+  print(my_list[0] +i, chr(my_list[0] +i), my_list[1] +i, chr(my_list[1] +i), my_list[2] +i, chr(my_list[2] +i))
+
+
+print("*"*20)
+
+# A második megoldás erősen "megerőszakolva", hogy csak a kisbetűket írja ki. Ennél biztos van szebb megoldás is, de ez legalább működik :-)
+
+my_list=[97,107,118]
+for i in range(5):
+  print(my_list[0] +i, chr(my_list[0] +i), my_list[1] +i, chr(my_list[1] +i), my_list[2] +i, chr(my_list[2] +i))
+my_list=[102,112]
+for i in range(5):
+  print(my_list[0] +i, chr(my_list[0] +i), my_list[1] +i, chr(my_list[1] +i))
