3ea5feed8c0a699d516ffcca408cf7f99f6ee626
Sirok Eszter <sirokeszter@gmail.com>
2021-06-22 00:04:05 +0200

    Selenium homework

--- selenium-homework/elementtype.py
+++ selenium-homework/elementtype.py
@@ -1,5 +1,37 @@
 from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+#options=Options()
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
+
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("http://localhost:9999/trickyelements.html")
+try:
+    elem1 = driver.find_element_by_id("element1")
+    elem2 = driver.find_element_by_id("element2")
+    elem3 = driver.find_element_by_id("element3")
+    elem4 = driver.find_element_by_id("element4")
+    elem5 = driver.find_element_by_id("element5")
+
+    button = driver.find_element_by_xpath('//button')
+    button.click()
+
+    if elem1 == button:
+        print(button.text)
+        if elem2 == button:
+            print(button.text)
+            if elem3 == button:
+                print(button.text)
+                if elem4 == button:
+                    print(button.text)
+                    if elem5 == button:
+                        print(button.text)
+
+    result = driver.find_element_by_id("result")
+    print(result.text)
+
+    assert(result.text==f"{button.text} was clicked")
+
+finally:
+    driver.close()
--- selenium-homework/seleniumex.py
+++ selenium-homework/seleniumex.py
@@ -4,5 +4,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://eupalyazatiportal.hu/")
 
-nemletezik=driver.find_element_by_id("nemletezik")
-nemletezik.click()
+nincs=driver.find_element_by_id("nemletezik")
+nincs.click()
