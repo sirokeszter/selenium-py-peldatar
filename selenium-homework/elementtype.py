4baa3e6a9f783cf91c11afdee8a6bcb791775de4
Sirok Eszter <sirokeszter@gmail.com>
2021-06-22 14:19:06 +0200

    Selenium homework

--- selenium-homework/elementtype.py
+++ selenium-homework/elementtype.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-#options=Options()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
 try:
     elem1 = driver.find_element_by_id("element1")
     elem2 = driver.find_element_by_id("element2")
@@ -14,24 +14,16 @@ try:
     elem4 = driver.find_element_by_id("element4")
     elem5 = driver.find_element_by_id("element5")
 
+    elemek= [elem1, elem2, elem3, elem4, elem5]
     button = driver.find_element_by_xpath('//button')
-    button.click()
 
-    if elem1 == button:
-        print(button.text)
-        if elem2 == button:
-            print(button.text)
-            if elem3 == button:
-                print(button.text)
-                if elem4 == button:
-                    print(button.text)
-                    if elem5 == button:
-                        print(button.text)
-
-    result = driver.find_element_by_id("result")
-    print(result.text)
-
-    assert(result.text==f"{button.text} was clicked")
+    for i in elemek:
+        if elemek[i]==button:
+            button.click()
+            result = driver.find_element_by_id("result")
+            print(result.text)
+            break
+            assert(result.text=="element{} was clicked".format(i))
 
 finally:
     driver.close()
