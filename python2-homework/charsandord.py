10611bf80615f509ef70ac5c6998fe8fc11d41f1
Sirok Eszter <sirokeszter@gmail.com>
2021-06-24 22:44:37 +0200

    Selenium homework

---
+++ python2-homework/charsandord.py
@@ -0,0 +1,20 @@
+import string
+
+text = open("text.txt", "r")
+
+dictionary = dict()
+
+for line in text:
+    line = line.strip()
+    line = line.lower()
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    words = line.split(" ")
+
+    for word in words:
+        if word in dictionary:
+            dictionary[word] = dictionary[word] + 1
+        else:
+            dictionary[word] = 1
+
+for key in list(dictionary.keys()):
+    print(key, dictionary[key])
