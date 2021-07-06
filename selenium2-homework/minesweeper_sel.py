6eec1c898a546c6153d9f54ea1f31b8c18310de5
Sirok Eszter <sirokeszter@gmail.com>
2021-07-06 20:02:22 +0200

    Selenium2 homework

---
+++ selenium2-homework/minesweeper_sel.py
@@ -0,0 +1,6 @@
+# A program töltse be a példatárból az http://localhost:9999/minesweeper-game.html oldalt.
+# A feladat egyszerű: éld túl az aknamezőt. A selenium python programnak kell automatikusan végigjátszania a játékot.
+# A megoldáshoz használhatsz más könyvtárakat is pl: https://github.com/madewokherd/mines
+
+# Ehhez a feladványhoz ennyit tudok hozzászólni:
+print('  _________\n /         \\\n |  /\\ /\\  |\n |    -    |\n |  \\___/  |\n \\_________/')
