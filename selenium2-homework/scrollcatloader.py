96197328617ec05e9ab903efae4a63efdc0b7b07
Sirok Eszter <sirokeszter@gmail.com>
2021-07-10 23:19:49 +0200

    20 id-t már kiírja itt is

---
+++ selenium2-homework/scrollcatloader.py
@@ -0,0 +1,37 @@
+# A program töltse be a példatárból az http://localhost:9999/scrolltoload.html oldalt. Mentsd le az első 20 macskás képet az oldalról.
+# A fájlokat egy külön cats könyvtárba mentsd le. A fájlneve legyen a következő {sorszam}_{cat_id} ahol a sorszám alatt azt értjük,
+# hogy hanyadiknak lett megjelenítve és cat_id meg az azonosító amit szolgáltató ad.
+# A {} jelek ne legyenek benne a fájlnévben. (ez a feladat majdnem ugyan az, mint az előző feladat, csakhogy nincs load more gomb.)
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
+import urllib.request
+
+options = Options()
+# options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+
+try:
+    driver.get("http://localhost:9999/scrolltoload.html")
+    html = driver.find_element_by_tag_name("html")
+    images = driver.find_elements_by_class_name("image")
+
+    for i in range(2):
+        time.sleep(3)
+        images = driver.find_elements_by_xpath("//div[@class='image']")
+        for j in images[0:10]:
+            url = j.find_element_by_tag_name("img").get_attribute("scr")
+            pict_name = j.find_element_by_tag_name("p").text
+            pname= pict_name.replace("Cat id:","")
+            print(pname)
+            #urllib.request.urlretrieve(url, f"{pname}.jpg")
+        html.send_keys(Keys.END)
+
+finally:
+    driver.close()
+
